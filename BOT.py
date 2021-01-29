2a09ca603805a3a91b5d10713e0d29e897beaba2
Vishnu Krishnan <50653457+Vishnukk33@users.noreply.github.com>
2021-01-29 18:37:04 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,12 +1,15 @@
-import pandas as pd
-import botometer
-import tweepy
-pd.options.mode.chained_assignment = None
+#Botometer API used to check if top 10 twitter accounts are bots
 
-consumer_key="Insert_consumer_key_here"
-consumer_secret="Insert_consumer_secret_here"
-access_token="Insert_access_token_here"
-access_token_secret="Insert_access_token_secret_here"
+import botometer
+import json
+import tweepy
+from beautifultable import BeautifulTable
+global api
+
+consumer_key="insert_consumer_key"
+consumer_secret="insert_consumer_secret_key"
+access_token="insert_access_token"
+access_token_secret="insert_access_token_secret"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -15,46 +18,64 @@ if (api):
 else:
     print("Failed")
     
-rapidapi_key = "Insert_rapidapi_key_here" 
+rapidapi_key = "insert rapid api key" # now it's called rapidapi key
 twitter_app_auth = {
     'consumer_key': consumer_key,
     'consumer_secret': consumer_secret,
     'access_token': access_token,
     'access_token_secret': access_token_secret,
   }
-
 bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
 
 
-user_name_list=['@narendramodi','@MetaphorMagnet','@DeepDrumpf']
-#Get the user name list and save it to user_name_list take max of 1999 and also add '@' symbol before using user = '@'+ user
+user_name_list= list(set(most_tweeted))
+user_name_list1= list(set(most_tweeted1))
 
-newcsvname="bot1.csv"
-df = pd.DataFrame(index=[0,1,2])
-df.to_csv(newcsvname)
-data=pd.read_csv(newcsvname)
-data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
-data['user']=" "
-data['bot']=" "
-data['probability']=0
-data.to_csv(newcsvname,index=False)
+print("Lets Check if the top Tweeters are Bots or not ")
+print("We take the top 2 tweeters on each day for republican and democratic parties over 5 days ")
+print("The results are as follows")
 
+table = BeautifulTable()
+table.columns.header = ["User", "Bot"]
+print("\n Top democratic tweeters:")
 for i in range (0,len(user_name_list)):
+    try:
+        bot_result = bom.check_account(user_name_list[i])
+        score=bot_result["cap"]["english"]*100
+        if (score>60):
+            bot_chance='Yes' 
+        if (score>40 and score<60):
+            bot_chance='Maybe'
+        if (score<40):
+            bot_chance='No'
+    except:
+        bot_chance='Unknown'
+        
+    table.rows.append([user_name_list[i],bot_chance])
 
-    print("Checking if user is a bot")
-    bot_result = bom.check_account(user_name_list[i])
-    score=bot_result["scores"]["english"]*100
+table.set_style(BeautifulTable.STYLE_GRID)
+print(table)
 
-    if (score>=40):
-        bot_chance='Yes'
-    elif (score<40):
-        bot_chance='No'
-   
-    data['user'][i]=user_name_list[i]
-    data['probability'][i]=score
-    data['bot'][i]=bot_chance
-   
-data.to_csv(newcsvname,index=False)
+table1 = BeautifulTable()
+table1.columns.header = ["User", "Bot"]
+print("\n Top republican tweeters:")
+for i in range (0,len(user_name_list1)):
+    try:
+        bot_result = bom.check_account(user_name_list1[i])
+        score=bot_result["cap"]["english"]*100
+        if (score>60):
+            bot_chance='Yes' 
+        if (score>40 and score<60):
+            bot_chance='Maybe'
+        if (score<40):
+            bot_chance='No'
+    except:
+        bot_chance='Unknown'
+        
+    table1.rows.append([user_name_list1[i],bot_chance])
+
+table1.set_style(BeautifulTable.STYLE_GRID)
+print(table1)
 
