fe55acba3248dc2dfec0c898f8d679438fa5fd4c
Vishnu Krishnan <50653457+Vishnukk33@users.noreply.github.com>
2021-01-29 18:29:23 +0530

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -1,22 +1,23 @@
+#Performing Preprocessing
+
 import pandas as pd
 import nltk
+import re
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-import re
 
 pd.options.mode.chained_assignment = None
 nltk.download('stopwords')
-csvname="book3.csv"
+csvname='ML.csv'
 data=pd.read_csv(csvname)
 data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 rows=len(data)
-corpus=[]
 
 # loop to clean the tweets and to remove stopwords and perform stemming.
 for i in range (0,rows):
     oldtext=data['Tweet'][i] 
-    newtext=' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",oldtext).split()) # removes hashtags, https links, usernames and any other alpha-numeric characters.
-    newtext= re.sub('[^a-zA-Z]'," ",newtext) #removes all other characters other than alphabets
+    newtext=' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",str(oldtext)).split()) # removes hashtags, https links, usernames and any other alpha-numeric characters.
+    newtext= re.sub('[^a-zA-Z]'," ",str(newtext)) #removes all other characters other than alphabets
     newtext=newtext.lower()
     newtext=newtext.split()
     ps=PorterStemmer()
@@ -24,6 +25,6 @@ for i in range (0,rows):
     newtext=' '.join(newtext)
     #print(newtext)
     data['Tweet'][i]=newtext
-    
+        
 # Saving changes to the csv file
-data.to_csv(csvname,index='False') 
+data.to_csv(csvname,index='False')
