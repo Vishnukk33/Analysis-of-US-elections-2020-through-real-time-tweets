ce9a61e3341540bf86e36ccff68328052462bf8f
Vishnu Krishnan <50653457+Vishnukk33@users.noreply.github.com>
2021-01-29 18:31:06 +0530

    Update ML_prediction.py

--- ML_prediction.py
+++ ML_prediction.py
@@ -1,3 +1,5 @@
+# Performing ML prediction.
+
 import re
 import nltk
 import pandas as pd
@@ -15,7 +17,7 @@ from xgboost import XGBClassifier
 from vecstack import stacking
 
 pd.options.mode.chained_assignment = None
-csvname="book3.csv" # Insert your csv name here.
+csvname="ML.csv" # Insert your csv name here.
 data=pd.read_csv(csvname)
 rows=len(data)
 
@@ -28,23 +30,20 @@ for i in range(rows):
     if(len(str((newtext)))!=0 and str(newtext)!='nan'):
         corpus.append(newtext)
         y.append(data['Party'][i])
-    print(i)
 
 # Using Term-Frequency-Iverse-Document-Frequency and fitting corpus into it.
-
+# Data-set 1 (X1) created using TF-IDF
 vectorizer = TfidfVectorizer(min_df=50,max_features=1500)
 vectorizer.fit(corpus)
 X1 = vectorizer.transform(corpus)
 X1=X1.toarray()
 
 # Using Count-Vectorizer and fitting corpus into it.
-
+# Data-set 2 (X2) created using Count-Vectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X2 = cv.fit_transform(corpus).toarray()
-
 y = np.array(y)
-
 # Splitting the dataset into Training and Test set.
 
 from sklearn.model_selection import train_test_split
@@ -54,11 +53,11 @@ X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y, test_size = 0.2, 
 
 # Performing LinearDiscriminantAnalysis.
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-lda = LDA(n_components =2)
+lda = LDA(n_components =1)
 X1_train = lda.fit_transform(X1_train, y1_train)
 X1_test = lda.transform(X1_test)
 
-lda = LDA(n_components =2)
+lda = LDA(n_components =1)
 X2_train = lda.fit_transform(X2_train, y2_train)
 X2_test = lda.transform(X2_test)
 
@@ -99,57 +98,51 @@ DT1.fit(X1_train, y1_train)
 DT2= DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 DT2.fit(X2_train, y2_train)
 
-# Performing Voting to improve accuracy. 
 
+#Predicting Outputs of the different classifiers
+
+y_pred=DT1.predict(X1_test)
+print("The Accuracy using Decision Tree on Data-set 1: ",accuracy_score(y1_test,y_pred))
+
+y_pred=DT2.predict(X2_test)
+print("The Accuracy using Decision Tree on Data-set 2: ",accuracy_score(y2_test,y_pred)) 
+
+y_pred=XGB1.predict(X1_test)
+print("The Accuracy using Extreme Gradient Boost on Data-set 1: ",accuracy_score(y1_test,y_pred))
+
+y_pred=XGB2.predict(X2_test)
+print("The Accuracy using Extreme Gradient Boost on Data-set 2: ",accuracy_score(y2_test,y_pred)) 
+
+y_pred=RF1.predict(X1_test)
+print("The Accuracy using Random Forest on Data-set 1: ",accuracy_score(y1_test,y_pred)) 
+
+y_pred=RF2.predict(X2_test)
+print("The Accuracy using Random Forest on Data-set 2: ",accuracy_score(y2_test,y_pred))
+
+y_pred=SVC1.predict(X1_test)
+print("The Accuracy using Support Vector Clustering on Data-set 1: ",accuracy_score(y1_test,y_pred))
+
+y_pred=SVC2.predict(X2_test)
+print("The Accuracy using Support Vector Clustering on Data-set 2: ",accuracy_score(y2_test,y_pred))
+
+y_pred=adaboostSVC1.predict(X1_test)
+print("The Accuracy using AdaBoosted Support Vector Clustering on Data-set 1: ",accuracy_score(y1_test,y_pred))
+
+y_pred=adaboostSVC2.predict(X2_test)
+print("The Accuracy using AdaBoosted Support Vector Clustering on Data-set 2: ",accuracy_score(y2_test,y_pred))
+
+
+# Performing Voting to improve accuracy. 
 from sklearn.ensemble import VotingClassifier
 
 eclf1 = VotingClassifier(estimators=[
      ('RF1',RF1),('adaboostSVC1',adaboostSVC1),('XGB1',XGB1)], voting='soft')
 eclf1.fit(X1_train, y1_train)
 y1_pred=eclf1.predict(X1_test)
-print(accuracy_score(y1_test,y1_pred)) #Output 0.6799696345298775
+print("The Accuracy using Voting Classifier on Data-set 1: ",accuracy_score(y1_test,y1_pred)) 
 
 eclf2 = VotingClassifier(estimators=[
      ('RF2',RF2),('adaboostSVC2',adaboostSVC2),('XGB2',XGB2)], voting='soft')
 eclf2.fit(X2_train, y2_train)
-y2_pred=eclf1.predict(X2_test)
-print(accuracy_score(y2_test,y2_pred)) #Output 0.6747641253660124
-
-"""eclf = VotingClassifier(estimators=[
-      ('eclf1',eclf1),('eclf2',eclf2)], voting='soft')
-
-eclf.fit(X2_train, y2_train)
-y_pred=eclf1.predict(X2_test)
-print(accuracy_score(y2_test,y_pred)) #Output 0.6747641253660124"""
-
-# Predicting using Individual Classifiers.
-
-y_pred=DT1.predict(X1_test)
-print(accuracy_score(y1_test,y_pred))
-
-y_pred=DT2.predict(X2_test)
-print(accuracy_score(y2_test,y_pred)) 
-
-y_pred=XGB1.predict(X1_test)
-print(accuracy_score(y1_test,y_pred)) 
-
-y_pred=XGB2.predict(X2_test)
-print(accuracy_score(y2_test,y_pred)) 
-
-y_pred=RF1.predict(X1_test)
-print(accuracy_score(y1_test,y_pred))
-
-y_pred=RF2.predict(X2_test)
-print(accuracy_score(y2_test,y_pred))
-
-y_pred=SVC1.predict(X1_test)
-print(accuracy_score(y1_test,y_pred))
-
-y_pred=SVC2.predict(X2_test)
-print(accuracy_score(y2_test,y_pred))
-
-y_pred=adaboostSVC1.predict(X1_test)
-print(accuracy_score(y1_test,y_pred))
-
-y_pred=adaboostSVC2.predict(X2_test)
-print(accuracy_score(y2_test,y_pred)) 
+y2_pred=eclf2.predict(X2_test)
+print("The Accuracy using Voting Classifier on Data-set 2: ",accuracy_score(y2_test,y2_pred))
